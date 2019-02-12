bc81700fbfe262d08057a8ac0e3df7ec69857834
Eric Teubert <eric@ericteubert.de>
2019-02-12 11:16:15 +0800

    Create .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,34 @@
+global_settings = "~/.iex.exs"
+if File.exists?(global_settings), do: Code.require_file(global_settings)
+
+Application.put_env(:elixir, :ansi_enabled, true)
+
+IEx.configure(
+  colors: [
+    eval_result: [:cyan, :bright],
+    eval_error: [[:red, :bright, "\n▶▶▶\n"]],
+    eval_info: [:yellow, :bright]
+  ],
+  default_prompt:
+    [
+      # cursor ⇒ column 1
+      "\e[G",
+      :white,
+      "%prefix",
+      :cyan,
+      "|",
+      :white,
+      "%counter",
+      " ",
+      :cyan,
+      "▶",
+      :reset
+    ]
+    |> IO.ANSI.format()
+    |> IO.chardata_to_string()
+)
+
+import Ecto.Query, warn: false
+
+alias Radiator.{Directory, Repo}
+alias Radiator.Directory.{Episode, Podcast}
