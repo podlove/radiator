82c08734a5ed80f92c5f8fd0ff4b1fde9fc5ba47
Eric Teubert <eric@ericteubert.de>
2019-02-13 12:12:18 +0800

    fix hal dependency

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Radiator.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:hal, path: "../hal"}
+      {:hal, "~> 1.1"}
     ]
   end
 
