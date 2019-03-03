824942bb8653e212f9405547cb86e65c6e99cbb2
Eric Teubert <eric@ericteubert.de>
2019-03-03 14:07:20 +0800

    remove sketches

--- lib/radiator_web/controllers/page_controller.ex
+++ lib/radiator_web/controllers/page_controller.ex
@@ -4,12 +4,4 @@ defmodule RadiatorWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
-
-  def sketch_podcasts(conn, _params) do
-    render(conn, "sketch_podcasts.html")
-  end
-
-  def sketch_podcasts_create(conn, _params) do
-    render(conn, "sketch_podcasts_create.html")
-  end
 end
