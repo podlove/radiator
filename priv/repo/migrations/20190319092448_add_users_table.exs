fdeb3598fe19d2fd6b97d1d589438d1ce9fc07f3
Dominik Wagner <wagnr@icloud.com>
2019-04-08 16:22:43 +0200

    change(auth): increment id sequence of user by a random amout on migration

--- priv/repo/migrations/20190319092448_add_users_table.exs
+++ priv/repo/migrations/20190319092448_add_users_table.exs
@@ -14,5 +14,14 @@ defmodule Radiator.Repo.Migrations.AddUsersTable do
 
     create unique_index(:auth_users, [:email])
     create unique_index(:auth_users, [:name])
+
+    # advance the users id by a random amount, but at least 10 to ensure the first 10 ids are free for special users if needed
+    # current plan is to make the id 1 a special public user
+    execute(
+      "select setval('auth_users_id_seq', nextval('auth_users_id_seq') + #{
+        :rand.uniform(999) + 10
+      })",
+      ""
+    )
   end
 end
