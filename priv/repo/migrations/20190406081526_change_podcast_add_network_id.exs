1fa620303c147950a583f92be1cf7776a7a26dab
Eric Teubert <eric@ericteubert.de>
2019-04-16 09:29:25 +0200

    chore: merge migrations

--- priv/repo/migrations/20190406081526_change_podcast_add_network_id.exs
+++
@@ -1,9 +0,0 @@
-defmodule Radiator.Repo.Migrations.ChangePodcastAddNetworkId do
-  use Ecto.Migration
-
-  def change do
-    alter table(:podcasts) do
-      add :network_id, references(:networks, on_delete: :nothing)
-    end
-  end
-end
