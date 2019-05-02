bf9ca6441f29a7b9c9af331747752afcd8513fd6
Eric Teubert <eric@ericteubert.de>
2019-05-02 10:17:14 +0200

    wip drone

--- config/test.exs
+++ config/test.exs
@@ -14,6 +14,7 @@ config :radiator, Radiator.Repo,
   username: "postgres",
   password: "postgres",
   database: "radiator_test",
+  # "localhost" on local, "database" on drone -- can I read this from an env?
   hostname: "database",
   pool: Ecto.Adapters.SQL.Sandbox
 
