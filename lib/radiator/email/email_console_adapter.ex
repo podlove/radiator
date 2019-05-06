b5db7a37c5bc62ce450b587dfad81d15f0880f7e
David Schilling <davejs92@gmail.com>
2019-05-06 21:50:49 +0200

    Use Logger.info instead of IO.puts

--- lib/radiator/email/email_console_adapter.ex
+++ lib/radiator/email/email_console_adapter.ex
@@ -2,13 +2,16 @@ defmodule Radiator.Email.Console do
   @moduledoc """
   Bamboo Adapter that logs emails to console.
   """
+
+  require Logger
+
   @behaviour Bamboo.Adapter
 
   def deliver(email, _config) do
     from_name= email.from |> elem(0)
     from_email= email.from |> elem(1)
-    IO.puts("Mail sent to #{from_name} <#{from_email}> with subject: '#{email.subject}'")
-    IO.puts(email.text_body)
+    Logger.info("Mail sent to #{from_name} <#{from_email}> with subject: '#{email.subject}'")
+    Logger.info(email.text_body)
   end
 
   def handle_config(config), do: config
