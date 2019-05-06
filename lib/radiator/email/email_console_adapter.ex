4c1af88fc8bcd77e860848071ba35a73b065c311
David Schilling <davejs92@gmail.com>
2019-05-06 21:52:10 +0200

    Run formatter

--- lib/radiator/email/email_console_adapter.ex
+++ lib/radiator/email/email_console_adapter.ex
@@ -8,8 +8,8 @@ defmodule Radiator.Email.Console do
   @behaviour Bamboo.Adapter
 
   def deliver(email, _config) do
-    from_name= email.from |> elem(0)
-    from_email= email.from |> elem(1)
+    from_name = email.from |> elem(0)
+    from_email = email.from |> elem(1)
     Logger.info("Mail sent to #{from_name} <#{from_email}> with subject: '#{email.subject}'")
     Logger.info(email.text_body)
   end
