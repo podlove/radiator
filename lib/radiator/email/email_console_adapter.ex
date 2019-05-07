37bba4ac378a4664e19ad468a5270489cb3c6c6f
David Schilling <davejs92@gmail.com>
2019-05-07 16:10:58 +0200

    Log emails to console in dev profile (#55)

---
+++ lib/radiator/email/email_console_adapter.ex
@@ -0,0 +1,20 @@
+defmodule Radiator.Email.Console do
+  @moduledoc """
+  Bamboo Adapter that logs emails to console.
+  """
+
+  require Logger
+
+  @behaviour Bamboo.Adapter
+
+  def deliver(email, _config) do
+    from_name = email.from |> elem(0)
+    from_email = email.from |> elem(1)
+    Logger.info("Mail sent to #{from_name} <#{from_email}> with subject: '#{email.subject}'")
+    Logger.info(email.text_body)
+  end
+
+  def handle_config(config), do: config
+
+  def supports_attachments?, do: true
+end
