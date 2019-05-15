f2121e767e0837b3a8dd71ee625b20b8f73e3fa7
Eric Teubert <eric@ericteubert.de>
2019-05-15 11:48:37 +0200

    upgrade tailwind to 1.0

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -2,8 +2,8 @@ var tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
-    require('precss'),
-    tailwindcss('./tailwind.js'),
+    // require('precss'),
+    tailwindcss('./tailwind.config.js'),
     require('autoprefixer')
   ]
 }
