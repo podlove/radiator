71d2209031d4d30c9fd333f6e81190ea3a6a96cf
Eric Teubert <eric@ericteubert.de>
2019-06-30 20:09:45 +0200

    fix missing server start

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,8 @@
 use Mix.Config
 
-config :radiator, RadiatorWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
+config :radiator, RadiatorWeb.Endpoint,
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 config :logger, level: :info
 
