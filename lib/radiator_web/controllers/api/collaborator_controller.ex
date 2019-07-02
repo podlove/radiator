b77d4f4b6b3b18f15a655b7a5d077d2814b4398a
Dominik Wagner <wagnr@icloud.com>
2019-07-02 14:19:25 +0200

    addressing feedback from review

--- lib/radiator_web/controllers/api/collaborator_controller.ex
+++ lib/radiator_web/controllers/api/collaborator_controller.ex
@@ -1,35 +1,49 @@
 defmodule RadiatorWeb.Api.CollaboratorController do
   use RadiatorWeb, :controller
-  use Radiator.Constants
+  use Radiator.Constants, :permissions
 
   alias Radiator.Directory.Editor
-
   alias Radiator.Directory.Collaborator
+  alias Radiator.Auth
 
   action_fallback RadiatorWeb.Api.FallbackController
 
   def create(conn, %{"username" => username, "permission" => permission} = params) do
     with actor <- current_user(conn),
-         {:ok, subject} <- current_subject(actor, params) do
-      case Radiator.Auth.Register.get_user_by_name(username) do
-        nil ->
-          {:error, :not_found}
-
-        user ->
-          collaborator = %Collaborator{
-            user: user,
-            permission: String.to_existing_atom(permission),
-            subject: subject
-          }
-
-          with {:ok, collaborator} <- Editor.add_collaborator(actor, collaborator) do
-            conn
-            |> put_status(:created)
-            |> render("show.json", %{collaborator: collaborator})
-          end
-      end
+         {:ok, subject} <- current_subject(actor, params),
+         user = %Auth.User{} <- Auth.Register.get_user_by_name(username),
+         {:ok, collaborator} <- build_collaborator(user, permission, subject),
+         {:ok, collaborator} <- Editor.add_collaborator(actor, collaborator) do
+      conn
+      |> put_status(:created)
+      |> render("show.json", %{collaborator: collaborator})
+    else
+      nil -> {:error, :not_found}
+      error -> error
     end
   end
+
+  defp build_collaborator(user, permission, subject) when is_permission(permission) do
+    {:ok,
+     %Collaborator{
+       user: user,
+       permission: permission,
+       subject: subject
+     }}
+  end
+
+  defp build_collaborator(user, permission, subject) when is_binary(permission) do
+    perm =
+      try do
+        String.to_existing_atom(permission)
+      catch
+        _, _ -> :invalid_permission
+      end
+
+    build_collaborator(user, perm, subject)
+  end
+
+  defp build_collaborator(_, _, _), do: {:error, :invalid_permission}
 
   def update(conn, %{"id" => username, "permission" => permission} = params) do
     with ^username <- params["username"],
