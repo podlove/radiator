b5a960e51e9c8ce4ee911423095e970a8c7058b3
Eric Teubert <eric@ericteubert.de>
2019-08-18 10:35:41 +0200

    fix tests (minio config)

--- config/test.exs
+++ config/test.exs
@@ -22,6 +22,16 @@ config :argon2_elixir, t_cost: 1, m_cost: 8
 
 config :arc,
   storage: Arc.Storage.S3,
+  asset_host: System.get_env("STORAGE_ASSET_HOST", "http://localhost:9000"),
   bucket: "radiator-test"
+
+config :ex_aws,
+  access_key_id: "IEKAZMUY3KX32CRJPE9R",
+  secret_access_key: "tXNYsfJyb8ctDgZSaIOYpndQwxOv8T+E+U0Rq3mN"
+
+config :ex_aws, :s3,
+  scheme: "http://",
+  host: System.get_env("STORAGE_HOST") || "localhost",
+  port: 9000
 
 config :radiator, Radiator.Mailer, adapter: Radiator.Email.Console
