4cac1098d2c81e2ab34c2d34c78b26467c8c0272
Dominik Wagner <wagnr@icloud.com>
2019-09-08 17:03:38 +0200

    fix: wrong type use of the new System.get_env/2

--- config/dev.exs
+++ config/dev.exs
@@ -34,7 +34,7 @@ config :ex_aws,
 config :ex_aws, :s3,
   scheme: "http://",
   host: System.get_env("STORAGE_HOST", default_host),
-  port: System.get_env("STORAGE_PORT", 9000)
+  port: System.get_env("STORAGE_PORT", "9000")
 
 # ## SSL Support
 #
