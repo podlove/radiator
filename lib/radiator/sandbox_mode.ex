5092acee6cd582b71364f5e9a4cf1ba70813f5f7
Eric Teubert <eric@ericteubert.de>
2019-09-26 14:45:26 +0200

    doc: rewrite SandboxMode doc

--- lib/radiator/sandbox_mode.ex
+++ lib/radiator/sandbox_mode.ex
@@ -1,17 +1,29 @@
 defmodule Radiator.SandboxMode do
   @moduledoc """
-  Global "Sandbox Mode".
+  A global sandbox mode for the Radiator instance.
 
-  Set via config
-
-      config :radiator, :sandbox_mode, enabled: false
-
-  Or in released per ENV by setting `SANDBOX_MODE_ENABLED` to true.
-
-  The following changes apply to a system in demo mode:
+  The following changes apply to a system in sandbox mode:
 
     - all feeds contain `<itunes:block>Yes</itunes:block>`
     - a robots.txt is generated disallowing all requests
+
+  Set via config
+
+  ```
+  config :radiator, :sandbox_mode, enabled: false
+  ```
+
+  Or in releases per ENV by setting `SANDBOX_MODE_ENABLED` to true.
+
+  Status can be queried by clients:
+
+  ```
+  query {
+    sandboxMode {
+      enabled
+    }
+  }
+  ```
   """
   def enabled? do
     Application.get_env(:radiator, :sandbox_mode)[:enabled] == true
