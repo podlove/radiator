f1792538607ef6d1036cf19d783627c4950ae877
Luka Dornhecker <luka.dornhecker@hey.com>
2023-11-14 23:06:01 +0100

    add users generated by mix phx.gen.auth

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Radiator.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:argon2_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
