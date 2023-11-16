12080f119f332245b6444bf4be4eb3c86d2d936f
sorax <robert@hausgedacht.de>
2023-11-16 10:23:20 +0100

    chore: add dep : mix_test_watch
    use TDD!
    mix test.watch --stale

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule Radiator.MixProject do
       {:floki, ">= 0.30.0", only: :test},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_dashboard, "~> 0.8.2"},
