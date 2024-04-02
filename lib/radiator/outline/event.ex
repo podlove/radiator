b1b3db443f89890b18ee664719aa5a362df5f73b
Martin Wöginger <martin.woeginger@gmail.com>
2024-04-02 21:10:16 +0200

    start with dummy event store

--- lib/radiator/outline/event.ex
+++ lib/radiator/outline/event.ex
@@ -2,6 +2,7 @@ defmodule Radiator.Outline.Event do
   @moduledoc false
 
   alias Radiator.Outline.Event.InsertNodeEvent
+  defstruct [:event_type, :event_id, :user_id, :payload]
 
   def build("insert_node", payload, user_id, event_id) do
     %InsertNodeEvent{
--- lib/radiator/outline/event_consumer.ex
+++ lib/radiator/outline/event_consumer.ex
@@ -6,6 +6,7 @@ defmodule Radiator.Outline.EventConsumer do
   alias Radiator.Outline
   alias Radiator.Outline.Event.InsertNodeEvent
   alias Radiator.Outline.EventProducer
+  alias Radiator.EventStore
 
   def start_link(opts \\ []) do
     GenStage.start_link(__MODULE__, opts, name: __MODULE__)
@@ -21,26 +22,21 @@ defmodule Radiator.Outline.EventConsumer do
     {:noreply, [], state}
   end
 
-  defp process_event(%InsertNodeEvent{payload: payload} = _event) do
+  defp process_event(%InsertNodeEvent{payload: payload} = event) do
     payload
     |> Outline.insert_node()
-    |> handle_insert_result()
-
-    #      validate
-    #         true->
-    #           database action: insert node()
-    #           create && persist event (event contains all attributes, user, event_id, timestamps)
-    #           broadcast event (topic: episode_id)
-    #           broadcast node (topic: episode_id)
-    #         false->
-    #           log error and return error (audit log)
+    |> handle_insert_result(event)
   end
 
-  defp handle_insert_result({:ok, node}) do
+  defp handle_insert_result({:ok, node}, event) do
+    EventStore.persist_event(event)
+
+    # persist event (event contains all attributes, user, event_id, timestamps)
+    # broadcast event (topic: episode_id)
     {:ok, node}
   end
 
-  defp handle_insert_result({:error, _error}) do
+  defp handle_insert_result({:error, _error}, _event) do
     # log_error_please :-)
 
     :error
