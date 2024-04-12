df4084384ca0555c4cc0c78e6d33a35e3197c4f9
Martin Wöginger <martin.woeginger@gmail.com>
2024-04-12 16:29:43 +0200

    WIP start with eventconsumer test

--- test/radiator/outline/event_consumer_test.exs
+++ test/radiator/outline/event_consumer_test.exs
@@ -1,4 +1,28 @@
 defmodule Radiator.Outline.EventConsumerTest do
+  use Radiator.DataCase
+
+  alias Radiator.Outline.Command
+  # alias Radiator.Outline.Command.InsertNodeCommand
+  alias Radiator.Outline.EventConsumer
+  alias Radiator.PodcastFixtures
+
   describe "handle_events/2" do
+    test "insert_node" do
+      episode = PodcastFixtures.episode_fixture()
+
+      attributes = %{
+        "title" => "Node Title",
+        "content" => "Node Content",
+        "episode_id" => episode.id
+      }
+
+      user_id = "user_id"
+      event_id = "event_id"
+
+      command = Command.build("insert_node", attributes, user_id, event_id)
+      EventConsumer.handle_events([command], 0, nil)
+      # assert a node has been created
+      # assert an event has been created (and be stored)
+    end
   end
 end
