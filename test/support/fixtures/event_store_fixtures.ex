6b5ccb7b23c2bb68c8eed8a5f34fa279b490f8c9
Martin Wöginger <martin.woeginger@gmail.com>
2024-05-21 20:05:06 +0200

    add persist_event tests plus cleanup, restructure

--- test/support/fixtures/event_store_fixtures.ex
+++ test/support/fixtures/event_store_fixtures.ex
@@ -5,6 +5,15 @@ defmodule Radiator.EventStoreFixtures do
   """
   alias Radiator.AccountsFixtures
 
+  alias Radiator.Outline.Event.{
+    NodeContentChangedEvent,
+    NodeDeletedEvent,
+    NodeInsertedEvent,
+    NodeMovedEvent
+  }
+
+  alias Radiator.OutlineFixtures
+
   @doc """
   Generate a event data.
   """
@@ -22,5 +31,50 @@ defmodule Radiator.EventStoreFixtures do
       |> Radiator.EventStore.create_event_data()
 
     event
+  end
+
+  def node_inserted_event_fixture(user_id: user_id) do
+    node = OutlineFixtures.node_fixture()
+
+    %NodeInsertedEvent{
+      node: node,
+      user_id: user_id,
+      event_id: Ecto.UUID.generate()
+    }
+  end
+
+  def node_content_changed_event_fixture(user_id: user_id) do
+    node = OutlineFixtures.node_fixture()
+
+    %NodeContentChangedEvent{
+      node_id: node.uuid,
+      content: node.content,
+      user_id: user_id,
+      event_id: Ecto.UUID.generate()
+    }
+  end
+
+  def node_deleted_event_fixture(user_id: user_id) do
+    node = OutlineFixtures.node_fixture()
+
+    %NodeDeletedEvent{
+      node_id: node.uuid,
+      user_id: user_id,
+      event_id: Ecto.UUID.generate()
+    }
+  end
+
+  def node_moved_event_fixture(user_id: user_id) do
+    node = OutlineFixtures.node_fixture()
+    parent = OutlineFixtures.node_fixture(episode_id: node.episode_id)
+    prev = OutlineFixtures.node_fixture(episode_id: node.episode_id)
+
+    %NodeMovedEvent{
+      node_id: node.uuid,
+      user_id: user_id,
+      parent_id: parent.uuid,
+      prev_id: prev.uuid,
+      event_id: Ecto.UUID.generate()
+    }
   end
 end
