a5300c07c34f921fc6c173020f366ed7b96c230e
Martin Wöginger <martin.woeginger@gmail.com>
2024-07-23 21:28:39 +0200

    WIP add sortable

--- assets/js/hooks/index.ts
+++ assets/js/hooks/index.ts
@@ -10,6 +10,8 @@ import {
   handleClean,
 } from "./events/handler";
 
+import Sortable from "../../vendor/sortable";
+
 export const Hooks = {
   outline: {
     mounted() {
@@ -29,6 +31,56 @@ export const Hooks = {
       this.handleEvent("change_content", handleContentChange.bind(this));
       this.handleEvent("delete", handleDelete.bind(this));
       this.handleEvent("clean", handleClean.bind(this));
+    },
+  },
+  sortable: {
+    mounted() {
+      let group = this.el.dataset.group;
+      let isDragging = false;
+      this.el.addEventListener(
+        "focusout",
+        (e) => isDragging && e.stopImmediatePropagation(),
+      );
+      let sorter = new Sortable(this.el, {
+        group: group ? { name: group, pull: true, put: true } : undefined,
+        animation: 150,
+        dragClass: "drag-item",
+        ghostClass: "drag-ghost",
+        onStart: (e) => (isDragging = true), // prevent phx-blur from firing while dragging
+        onEnd: (e) => {
+          isDragging = false;
+          let params = {
+            old: e.oldIndex,
+            new: e.newIndex,
+            to: e.to.dataset,
+            ...e.item.dataset,
+          };
+          this.pushEventTo(
+            this.el,
+            this.el.dataset["drop"] || "reposition",
+            params,
+          );
+        },
+      });
+    },
+  },
+  sortableInputFor: {
+    mounted() {
+      let group = this.el.dataset.group;
+      let sorter = new Sortable(this.el, {
+        group: group ? { name: group, pull: true, put: true } : undefined,
+        animation: 150,
+        dragClass: "drag-item",
+        ghostClass: "drag-ghost",
+        handle: "[data-handle]",
+        forceFallback: true,
+        onEnd: (e) => {
+          this.el
+            .closest("form")
+            .querySelector("input")
+            .dispatchEvent(new Event("input", { bubbles: true }));
+        },
+      });
     },
   },
 };
