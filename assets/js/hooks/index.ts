257fa7cc6bf5e8d500e1f11f5e88c31e71a82c32
Mathias Wingert <mathiaswingert@gmail.com>
2024-09-11 16:29:39 +0200

    feature: display all editing users in different colors
    - each user has it's own <span> tag
    - each user name is displayed in one of 12 colors * 3 intensities = 36
    - color selection based on first char and user name length

--- assets/js/hooks/index.ts
+++ assets/js/hooks/index.ts
@@ -1,17 +1,22 @@
 import { moveNode, setAttribute } from "./node";
-import { getItemById } from "./item";
+import {
+  getItemById,
+  addEditingUserLabel,
+  removeEditingUserLabel,
+} from "./item";
 
 export const Hooks = {
   outline: {
     selector: '.node:not([data-processed="true"]',
     mounted() {
       this.handleEvent("focus", ({ uuid, user_name }) => {
-        const item = getItemById(uuid);
-        item!.querySelector(".editing")!.innerHTML = user_name;
+        const item = getItemById(uuid)!;
+        addEditingUserLabel(item, user_name);
       });
-      this.handleEvent("blur", ({ uuid }) => {
-        const item = getItemById(uuid);
-        item!.querySelector(".editing")!.innerHTML = "";
+
+      this.handleEvent("blur", ({ uuid, user_name }) => {
+        const item = getItemById(uuid)!;
+        removeEditingUserLabel(item, user_name);
       });
 
       this.handleEvent("move_node", ({ uuid, parent_id, prev_id }) => {
