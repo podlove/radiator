8086d7db223b88b1c3d3786b8c86ecac696b3ee2
Martin Wöginger <martin.woeginger@gmail.com>
2024-10-17 21:51:40 +0200

    configure raindrop client

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,12 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :radiator, :service,
+  raindrop: %{
+    client_id: System.get_env("RAINDROP_CLIENT_ID"),
+    client_secret: System.get_env("RAINDROP_CLIENT_SECRET")
+  }
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- config/runtime.exs
+++ config/runtime.exs
@@ -53,6 +53,9 @@ if config_env() == :prod do
 
   config :radiator, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
+  config :radiator, :raindrop_client_id, System.get_env("RAINDROP_CLIENT_ID")
+  config :radiator, :raindrop_client_secret, System.get_env("RAINDROP_CLIENT_SECRET")
+
   config :radiator, RadiatorWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
@@ -64,56 +67,6 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :radiator, RadiatorWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :radiator, RadiatorWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :radiator, Radiator.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 
   config :radiator, Radiator.Mailer,
     adapter: Swoosh.Adapters.SMTP,
