564b32a429dc9bdf55fc5e5d85fd3b62d268d1e5
sorax <robert@hausgedacht.de>
2024-10-20 11:04:41 +0200

    extract outline input to component

--- lib/radiator_web/components/outline_components.ex
+++ lib/radiator_web/components/outline_components.ex
@@ -4,6 +4,8 @@ defmodule RadiatorWeb.OutlineComponents do
   """
   use Phoenix.Component
 
+  alias Phoenix.HTML
+
   alias RadiatorWeb.CoreComponents, as: Core
 
   alias Radiator.Outline
@@ -48,6 +50,153 @@ defmodule RadiatorWeb.OutlineComponents do
         </div>
       </div>
     </.form>
+    """
+  end
+
+  @doc """
+  Renders an input with label and error messages.
+
+  A `Phoenix.HTML.FormField` may be passed as argument,
+  which is used to retrieve the input name, id, and values.
+  Otherwise all attributes may be passed explicitly.
+
+  ## Types
+
+  This function accepts all HTML input types, considering that:
+
+    * You may also set `type="select"` to render a `<select>` tag
+
+    * `type="checkbox"` is used exclusively to render boolean values
+
+    * For live file uploads, see `Phoenix.Component.live_file_input/1`
+
+  See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+  for more information. Unsupported types, such as hidden and radio,
+  are best written directly in your templates.
+
+  ## Examples
+
+      <.input field={@form[:email]} type="email" />
+      <.input name="my-input" errors={["oh no!"]} />
+  """
+  attr :id, :any, default: nil
+  attr :name, :any
+  attr :label, :string, default: nil
+  attr :value, :any
+
+  attr :type, :string,
+    default: "text",
+    values: ~w(checkbox color date datetime-local email file month number password
+               range search select tel text textarea time url week)
+
+  attr :field, HTML.FormField,
+    doc: "a form field struct retrieved from the form, for example: @form[:email]"
+
+  attr :errors, :list, default: []
+  attr :checked, :boolean, doc: "the checked flag for checkbox inputs"
+  attr :prompt, :string, default: nil, doc: "the prompt for select inputs"
+  attr :options, :list, doc: "the options to pass to Phoenix.HTML.Form.options_for_select/2"
+  attr :multiple, :boolean, default: false, doc: "the multiple flag for select inputs"
+  attr :class, :string, default: nil
+
+  attr :rest, :global,
+    include: ~w(accept autocomplete capture cols disabled form list max maxlength min minlength
+                multiple pattern placeholder readonly required rows size step)
+
+  def outline_input(%{field: %HTML.FormField{} = field} = assigns) do
+    errors = if Phoenix.Component.used_input?(field), do: field.errors, else: []
+
+    assigns
+    |> assign(field: nil, id: assigns.id || field.id)
+    |> assign(:errors, Enum.map(errors, &Core.translate_error(&1)))
+    |> assign_new(:name, fn -> if assigns.multiple, do: field.name <> "[]", else: field.name end)
+    |> assign_new(:value, fn -> field.value end)
+    |> outline_input()
+  end
+
+  def outline_input(%{type: "checkbox"} = assigns) do
+    assigns =
+      assign_new(assigns, :checked, fn ->
+        HTML.Form.normalize_value("checkbox", assigns[:value])
+      end)
+
+    ~H"""
+    <div class={@class}>
+      <label class="flex items-center gap-4 text-sm leading-6 text-zinc-600">
+        <input type="hidden" name={@name} value="false" disabled={@rest[:disabled]} />
+        <input
+          type="checkbox"
+          id={@id}
+          name={@name}
+          value="true"
+          checked={@checked}
+          class="rounded border-zinc-300 text-zinc-900 focus:ring-0"
+          {@rest}
+        />
+        <%= @label %>
+      </label>
+      <Core.error :for={msg <- @errors}><%= msg %></Core.error>
+    </div>
+    """
+  end
+
+  def outline_input(%{type: "select"} = assigns) do
+    ~H"""
+    <div class={@class}>
+      <Core.label for={@id}><%= @label %></Core.label>
+      <select
+        id={@id}
+        name={@name}
+        class="block w-full mt-2 bg-white border border-gray-300 rounded-md shadow-sm focus:border-zinc-400 focus:ring-0 sm:text-sm"
+        multiple={@multiple}
+        {@rest}
+      >
+        <option :if={@prompt} value=""><%= @prompt %></option>
+        <%= HTML.Form.options_for_select(@options, @value) %>
+      </select>
+      <Core.error :for={msg <- @errors}><%= msg %></Core.error>
+    </div>
+    """
+  end
+
+  def outline_input(%{type: "textarea"} = assigns) do
+    ~H"""
+    <div class={@class}>
+      <Core.label for={@id}><%= @label %></Core.label>
+      <textarea
+        id={@id}
+        name={@name}
+        class={[
+          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 min-h-[6rem]",
+          @errors == [] && "border-zinc-300 focus:border-zinc-400",
+          @errors != [] && "border-rose-400 focus:border-rose-400"
+        ]}
+        {@rest}
+      ><%= HTML.Form.normalize_value("textarea", @value) %></textarea>
+      <Core.error :for={msg <- @errors}><%= msg %></Core.error>
+    </div>
+    """
+  end
+
+  # All other inputs text, datetime-local, url, password, etc. are handled here...
+  def outline_input(assigns) do
+    ~H"""
+    <div class={@class}>
+      <Core.label for={@id}><%= @label %></Core.label>
+      <input
+        type={@type}
+        name={@name}
+        id={@id}
+        value={HTML.Form.normalize_value(@type, @value)}
+        class={[
+          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          @errors == [] && "border-zinc-300 focus:border-zinc-400",
+          @errors != [] && "border-rose-400 focus:border-rose-400"
+        ]}
+        {@rest}
+      />
+      <Core.error :for={msg <- @errors}><%= msg %></Core.error>
+    </div>
     """
   end
 
