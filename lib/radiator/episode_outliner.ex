d26b0de5c74f55784333edb34d7ee2bcdea61b58
Martin Wöginger <martin.woeginger@gmail.com>
2024-12-29 14:28:06 +0100

    cleanup

--- lib/radiator/episode_outliner.ex
+++ lib/radiator/episode_outliner.ex
@@ -6,14 +6,6 @@ defmodule Radiator.EpisodeOutliner do
   alias Radiator.Podcast
   alias Radiator.Podcast.Episode
 
-  def insert_node(%{"episode_id" => episode_id} = attrs) do
-    %Episode{outline_node_container: outline_node_container} = Podcast.get_episode!(episode_id)
-
-    attrs
-    |> Map.put("outline_node_container", outline_node_container)
-    |> insert_node()
-  end
-
   def episode_id_for_node(node) do
     node
     |> episode_for_node()
@@ -22,10 +14,6 @@ defmodule Radiator.EpisodeOutliner do
 
   def episode_for_node(%Node{outline_node_container_id: outline_node_container_id}) do
     Podcast.get_episode_by_container_id(outline_node_container_id)
-    # case Podcast.get_episode_by_container_id(outline_node_container_id) do
-    #   {:ok, episode} -> episode
-    #   _ -> nil
-    # end
   end
 
   defp episode_id(nil), do: nil
