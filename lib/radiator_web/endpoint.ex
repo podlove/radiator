29fcc3a5a647ff1bee6c2b1872799465726997b3
Robert <333068+sorax@users.noreply.github.com>
2025-05-27 20:56:58 +0200

    add tidewave (#614)

--- lib/radiator_web/endpoint.ex
+++ lib/radiator_web/endpoint.ex
@@ -25,6 +25,10 @@ defmodule RadiatorWeb.Endpoint do
     gzip: false,
     only: RadiatorWeb.static_paths()
 
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
