d1587efff87b1eb64b8a3a6e7746f864b897c6b2
sorax <robert@hausgedacht.de>
2025-06-17 21:55:03 +0200

    make credo even more happy

--- test/support/feature_case.ex
+++ test/support/feature_case.ex
@@ -3,6 +3,8 @@ defmodule RadiatorWeb.FeatureCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       use RadiatorWeb, :verified_routes
@@ -14,8 +16,8 @@ defmodule RadiatorWeb.FeatureCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Radiator.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Radiator.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
