42696d618285e975f23f31a0b2a0fa17845eec9f
Luka Dornhecker <luka@jibbe.rs>
2025-09-14 17:41:34 +0200

    add Radiator.Podcasts domain

--- config/config.exs
+++ config/config.exs
@@ -44,7 +44,8 @@ config :spark,
 
 config :radiator,
   ecto_repos: [Radiator.Repo],
-  generators: [timestamp_type: :utc_datetime]
+  generators: [timestamp_type: :utc_datetime],
+  ash_domains: [Radiator.Podcasts]
 
 # Configures the endpoint
 config :radiator, RadiatorWeb.Endpoint,
---
+++ lib/radiator/podcasts.ex
@@ -0,0 +1,7 @@
+defmodule Radiator.Podcasts do
+  use Ash.Domain,
+    otp_app: :radiator
+
+  resources do
+  end
+end
