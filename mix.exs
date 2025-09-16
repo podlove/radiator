c42df2f95f60582ea08260cbca71f5292f7db240
Tim Pritlove <tim@pritlove.org>
2025-09-16 17:15:03 +0200

    update to metalove 0.5.0 from hex

--- mix.exs
+++ mix.exs
@@ -72,7 +72,7 @@ defmodule Radiator.MixProject do
        depth: 1},
       {:swoosh, "~> 1.16"},
       {:req, "~> 0.5"},
-      {:metalove, github: "podlove/metalove", branch: "deps-update"},
+      {:metalove, "~> 0.5.0"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
