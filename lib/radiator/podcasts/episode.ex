26f9b502037f2ad9ea671cb6d2b332580475160a
Martin Wöginger <martin.woeginger@gmail.com>
2026-03-04 22:32:25 +0100

    fixuo after rebase

--- lib/radiator/podcasts/episode.ex
+++ lib/radiator/podcasts/episode.ex
@@ -17,11 +17,11 @@ defmodule Radiator.Podcasts.Episode do
   alias Radiator.Podcasts.Track
 
   postgres do
-    table("episodes")
-    repo(Radiator.Repo)
+    table "episodes"
+    repo Radiator.Repo
 
     references do
-      reference(:podcast, on_delete: :delete)
+      reference :podcast, on_delete: :delete
     end
   end
 
@@ -47,98 +47,97 @@ defmodule Radiator.Podcasts.Episode do
   ]
 
   actions do
-    defaults([:read, :destroy])
-    default_accept(@default_accept_attributes)
+    defaults [:read, :destroy]
+    default_accept @default_accept_attributes
 
     create :create do
-      accept(@default_accept_attributes ++ [:podcast_id])
+      accept @default_accept_attributes ++ [:podcast_id]
     end
 
     create :import do
-      description("Import an episode from external feed data")
-      accept(@default_accept_attributes ++ [:guid, :podcast_id])
+      description "Import an episode from external feed data"
+      accept @default_accept_attributes ++ [:guid, :podcast_id]
     end
 
     update :update do
-      require_atomic?(false)
-      argument(:participants, {:array, :map}, allow_nil?: true)
-      argument(:add_participant, :struct, allow_nil?: true, constraints: [instance_of: Persona])
+      require_atomic? false
+      argument :participants, {:array, :map}, allow_nil?: true
+      argument :add_participant, :struct, allow_nil?: true, constraints: [instance_of: Persona]
 
-      argument(:remove_participant, :struct,
+      argument :remove_participant, :struct,
         allow_nil?: true,
         constraints: [instance_of: Persona]
-      )
 
-      change(manage_relationship(:participants, type: :append_and_remove))
-      change(manage_relationship(:add_participant, :participants, type: :append))
-      change(manage_relationship(:remove_participant, :participants, type: :remove))
+      change manage_relationship(:participants, type: :append_and_remove)
+      change manage_relationship(:add_participant, :participants, type: :append)
+      change manage_relationship(:remove_participant, :participants, type: :remove)
     end
 
     update :begin_scheduling do
-      description("Transition episode to scheduling state")
-      change(transition_state(:scheduling))
+      description "Transition episode to scheduling state"
+      change transition_state(:scheduling)
     end
 
     update :finalize_scheduling do
-      description("Transition episode to scheduled state after scheduling is complete")
-      accept([:publication_date])
-      change(transition_state(:scheduled))
+      description "Transition episode to scheduled state after scheduling is complete"
+      accept [:publication_date]
+      change transition_state(:scheduled)
     end
 
     update :back_to_scheduling do
-      description("Reopen scheduling for an episode")
-      change(transition_state(:scheduling))
+      description "Reopen scheduling for an episode"
+      change transition_state(:scheduling)
     end
   end
 
   attributes do
-    uuid_primary_key(:id)
+    uuid_primary_key :id
 
     attribute :guid, :string do
-      description("The unique identifier for the episode")
-      allow_nil?(false)
-      public?(true)
-      default(&Ash.UUID.generate/0)
+      description "The unique identifier for the episode"
+      allow_nil? false
+      public? true
+      default &Ash.UUID.generate/0
     end
 
     attribute :title, :string do
-      description("An episode's title")
-      allow_nil?(false)
-      public?(true)
+      description "An episode's title"
+      allow_nil? false
+      public? true
     end
 
     attribute :subtitle, :string do
-      description("An episode's subtitle")
-      public?(true)
+      description "An episode's subtitle"
+      public? true
     end
 
     attribute :summary, :string do
-      description("An episode's summary")
-      public?(true)
-      constraints(max_length: 4000)
+      description "An episode's summary"
+      public? true
+      constraints max_length: 4000
     end
 
     attribute :number, :integer do
-      description("An episode's number")
-      allow_nil?(true)
-      public?(true)
+      description "An episode's number"
+      allow_nil? true
+      public? true
     end
 
     attribute :itunes_type, Radiator.Podcasts.ItunesEpisodeType do
-      description("The iTunes type of the episode")
-      allow_nil?(false)
-      public?(true)
-      default(:full)
+      description "The iTunes type of the episode"
+      allow_nil? false
+      public? true
+      default :full
     end
 
     attribute :publication_date, :utc_datetime do
-      description("The date and time the episode was published")
-      public?(true)
+      description "The date and time the episode was published"
+      public? true
     end
 
     attribute :duration_ms, :integer do
-      description("The duration of the episode in milliseconds")
-      public?(true)
+      description "The duration of the episode in milliseconds"
+      public? true
     end
 
     timestamps()
@@ -146,38 +145,38 @@ defmodule Radiator.Podcasts.Episode do
 
   relationships do
     belongs_to :podcast, Podcast do
-      description("The podcast this episode belongs to")
-      public?(true)
-      allow_nil?(false)
+      description "The podcast this episode belongs to"
+      public? true
+      allow_nil? false
     end
 
     has_many :chapters, Chapter do
-      description("The chapters of the episode")
-      public?(true)
-      sort(start_time_ms: :asc)
+      description "The chapters of the episode"
+      public? true
+      sort start_time_ms: :asc
     end
 
     many_to_many :participants, Persona do
-      through(EpisodeParticipant)
-      public?(true)
+      through EpisodeParticipant
+      public? true
     end
 
-    has_many(:tracks, Track)
+    has_many :tracks, Track
 
     has_one :scheduling, Radiator.Podcasts.Episode.Scheduling do
-      description("The scheduling information for this episode")
-      public?(true)
+      description "The scheduling information for this episode"
+      public? true
     end
   end
 
   identities do
     identity :guid, [:guid] do
-      eager_check?(true)
+      eager_check? true
     end
 
     # HINT: add season_id to the identity when seasons are addeed
     identity :number, [:number, :podcast_id] do
-      eager_check?(true)
+      eager_check? true
     end
   end
 end
